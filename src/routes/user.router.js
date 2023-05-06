d65383c91d2247ff0b65be98eb47d3c3051e8360
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-06 18:31:26 -0500

    Update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,7 +5,7 @@ const verifyJWT = require('../middleware/auth.middleware');
 const userRouter = express.Router();
 
 userRouter.route('/')
-    .get(getAll)
+    .get(verifyJWT, getAll)
     .post(create);
     
 userRouter.route("/logged")
@@ -27,8 +27,8 @@ userRouter.route("/verify/:token")
     .get(verifyEmail)
 
 userRouter.route('/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
 
 module.exports = userRouter;
